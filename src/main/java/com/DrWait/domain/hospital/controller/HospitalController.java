58597ff6f93ea9fb0cd478f6eed0538e1f4728b5
ETi0n <ysl04030@gmail.com>
2025-05-31 15:04:47 +0900

    fix: 사용자/병원 정보 조회 토큰으로 빼도록 수정

--- src/main/java/com/DrWait/domain/hospital/controller/HospitalController.java
+++ src/main/java/com/DrWait/domain/hospital/controller/HospitalController.java
@@ -1,9 +1,14 @@
 package com.DrWait.domain.hospital.controller;
 
 import com.DrWait.domain.hospital.dto.HospitalInfoResponseDto;
+import com.DrWait.domain.hospital.entity.Hospital;
 import com.DrWait.domain.hospital.service.HospitalService;
 import com.DrWait.domain.user.service.HospitalPrincipal;
+import com.DrWait.global.security.auth.service.AuthService;
+import com.DrWait.global.security.token.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HospitalController {
 
     private final HospitalService hospitalService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthService authService;
 
     @GetMapping("")
-    public ResponseEntity<HospitalInfoResponseDto> getHospitalInfo(@AuthenticationPrincipal HospitalPrincipal hospital){
-        return ResponseEntity.ok(hospitalService.getHospitalInfo(hospital.getUsername()));
+    public ResponseEntity<HospitalInfoResponseDto> getHospitalInfo(HttpServletRequest request){
+        String token = jwtTokenProvider.resolveToken(request);
+        if(token == null || !jwtTokenProvider.validateToken(token)){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        Hospital hospital = authService.getHospitalByBearerToken(token);
+        return ResponseEntity.ok(hospitalService.getHospitalInfo(hospital));
     }
 }
