3bfe84403173c366a29f8c21414f86095d256052
Ichiya Luna <ichiyaluna@yonsei.ac.kr>
2025-06-01 21:24:23 +0900

    🔥 HOTFIX

--- src/main/java/com/DrWait/domain/reservation/dto/MyReservationDto.java
+++ src/main/java/com/DrWait/domain/reservation/dto/MyReservationDto.java
@@ -1,5 +1,6 @@
 package com.DrWait.domain.reservation.dto;
 
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,14 +8,17 @@ import lombok.Data;
 @AllArgsConstructor
 public class MyReservationDto {
 
-    private String userFullname;
-    private String role;
-    private boolean firstVisit;
-    private String message;
-    private String symptomName;
-    private String reservationTime;
-    private boolean completed;
-    private boolean confirmed;
-    private int waitingOrder;
-    private int waitingTime;
+  private Long reservationId;
+  private UUID hospitalId;
+  private UUID userId;
+  private String userFullname;
+  private String role;
+  private boolean firstVisit;
+  private String message;
+  private String symptomName;
+  private String reservationTime;
+  private boolean completed;
+  private boolean confirmed;
+  private int waitingOrder;
+  private int waitingTime;
 }
--- src/main/java/com/DrWait/domain/reservation/service/ReservationService.java
+++ src/main/java/com/DrWait/domain/reservation/service/ReservationService.java
@@ -7,84 +7,86 @@ import com.DrWait.domain.reservation.entity.ReservationEntity;
 import com.DrWait.domain.reservation.repository.ReservationRepository;
 import com.DrWait.domain.symptom.repository.SymptomRepository;
 import com.DrWait.domain.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 // 순서 계산 (쿼리) -> is_confirmed && !is_completed order by reservation_time asc
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
 
-    private final ReservationRepository reservationRepository;
-    private final UserRepository userRepository;
-    private final SymptomRepository symptomRepository;
+  private final ReservationRepository reservationRepository;
+  private final UserRepository userRepository;
+  private final SymptomRepository symptomRepository;
 
-    public ReservationResultDto makeReservation(ReservationDto dto, UUID userId) {
-        ReservationEntity entity = new ReservationEntity();
-        entity.setHospitalId(dto.getHospitalId());
-        entity.setUserId(userId);
-        entity.setRole(dto.getRole());
-        entity.setSymptomId(dto.getSymptomId());
-        entity.setFirstVisit(dto.isFirstVisit());
-        entity.setConfirmed(false);
-        entity.setCompleted(false);   // 처음 생성 시 false
-        entity.setMessage(dto.getMessage());
-        entity.setReservationTime(LocalDateTime.now());
+  public ReservationResultDto makeReservation(ReservationDto dto, UUID userId) {
+    ReservationEntity entity = new ReservationEntity();
+    entity.setHospitalId(dto.getHospitalId());
+    entity.setUserId(userId);
+    entity.setRole(dto.getRole());
+    entity.setSymptomId(dto.getSymptomId());
+    entity.setFirstVisit(dto.isFirstVisit());
+    entity.setConfirmed(false);
+    entity.setCompleted(false);   // 처음 생성 시 false
+    entity.setMessage(dto.getMessage());
+    entity.setReservationTime(LocalDateTime.now());
 
-        reservationRepository.save(entity);
+    reservationRepository.save(entity);
 
-        int waitingOrder = reservationRepository
-                .countByHospitalIdAndIsConfirmedTrueAndIsCompletedFalseAndReservationTimeBefore(
-                        entity.getHospitalId(),
-                        entity.getReservationTime()
-                ) +1;
+    int waitingOrder = reservationRepository
+        .countByHospitalIdAndIsConfirmedTrueAndIsCompletedFalseAndReservationTimeBefore(
+            entity.getHospitalId(),
+            entity.getReservationTime()
+        ) + 1;
 
-        int waitingTime = (waitingOrder - 1) * 5;
-        if (waitingOrder == 0) {
-            waitingTime = 0;
-        }
-
-        return new ReservationResultDto(waitingOrder, waitingTime);
+    int waitingTime = (waitingOrder - 1) * 5;
+    if (waitingOrder == 0) {
+      waitingTime = 0;
     }
 
-    public void cancelReservation(Long reservationId) {
-        reservationRepository.deleteById(reservationId);
-    }
+    return new ReservationResultDto(waitingOrder, waitingTime);
+  }
 
-    public List<MyReservationDto> getUserReservationDetails(UUID userId) {
-        List<ReservationEntity> reservations = reservationRepository.findAllByUserId(userId);
+  public void cancelReservation(Long reservationId) {
+    reservationRepository.deleteById(reservationId);
+  }
 
-        return reservations.stream().map(entity -> {
-            String userFullname = userRepository.findById(userId).get().getFullname();
-            String symptomName = symptomRepository.findById(entity.getSymptomId()).get().getName();
+  public List<MyReservationDto> getUserReservationDetails(UUID userId) {
+    List<ReservationEntity> reservations = reservationRepository.findAllByUserId(userId);
 
-            int waitingOrder = reservationRepository
-                    .countByHospitalIdAndIsConfirmedTrueAndIsCompletedFalseAndReservationTimeBefore(
-                            entity.getHospitalId(),
-                            entity.getReservationTime()
-                    ) + 1;
+    return reservations.stream().map(entity -> {
+      String userFullname = userRepository.findById(userId).get().getFullname();
+      String symptomName = symptomRepository.findById(entity.getSymptomId()).get().getName();
 
-            int waitingTime = (waitingOrder - 1) * 5;
+      int waitingOrder = reservationRepository
+          .countByHospitalIdAndIsConfirmedTrueAndIsCompletedFalseAndReservationTimeBefore(
+              entity.getHospitalId(),
+              entity.getReservationTime()
+          ) + 1;
 
-            return new MyReservationDto(
-                    userFullname,
-                    entity.getRole(),
-                    entity.isFirstVisit(),
-                    entity.getMessage(),
-                    symptomName,
-                    entity.getReservationTime().toString(),
-                    entity.isCompleted(),
-                    entity.isConfirmed(),
-                    waitingOrder,
-                    waitingTime
-            );
-        }).collect(Collectors.toList());
-    }
+      int waitingTime = (waitingOrder - 1) * 5;
+
+      return new MyReservationDto(
+          entity.getId(),
+          entity.getHospitalId(),
+          entity.getUserId(),
+          userFullname,
+          entity.getRole(),
+          entity.isFirstVisit(),
+          entity.getMessage(),
+          symptomName,
+          entity.getReservationTime().toString(),
+          entity.isCompleted(),
+          entity.isConfirmed(),
+          waitingOrder,
+          waitingTime
+      );
+    }).collect(Collectors.toList());
+  }
 
 
 }
